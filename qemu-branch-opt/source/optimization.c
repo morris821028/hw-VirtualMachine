28baca3de49e2d905c3f441afc3722aa14c52d6d
unknown <morris821028@gmail.com>
2016-03-29 10:13:08 +0800

    finish indirect branch cache

--- qemu-branch-opt/source/optimization.c
+++ qemu-branch-opt/source/optimization.c
@@ -13,6 +13,8 @@
 
 extern uint8_t *optimization_ret_addr;
 
+#define HW_OPT_IBTC
+#define HW_OPT_SHACK
 /*
  * Shadow Stack
  */
@@ -20,23 +22,38 @@ list_t *shadow_hash_list;
 
 static inline void shack_init(CPUState *env)
 {
+#ifdef HW_OPT_SHACK
 	/* Create shadow stack */
 	env->shack = (uint64_t *) calloc(SHACK_SIZE, sizeof(uint64_t));
 	env->shack_top = env->shack;
 	env->shack_end = env->shack + SHACK_SIZE;
-	env->shadow_hash_list = (void *) calloc(TB_JMP_CACHE_SIZE, sizeof(list_t));
-	env->shadow_ret_count = 0;
-	env->shadow_ret_addr = 
-				(unsigned long *) calloc(SHACK_SIZE, sizeof(unsigned long));
+	env->shack_hash = (struct shack_hash*) calloc(1, sizeof(struct shack_hash));
+#endif
 }
 
 /*
  * shack_set_shadow()
  *  Insert a guest eip to host eip pair if it is not yet created.
  */
+static shack_slot* shack_hash_find(CPUState *env, target_ulong guest_eip) {
+	target_ulong h = guest_eip & SHACK_MASK;
+	shack_slot *p = env->shack_hash->head[h];
+	for (; p != NULL; p = p->next) {
+		if (p->guest_eip == guest_eip)
+			return p;
+	}
+	p = (shack_slot *) calloc(1, sizeof(shack_slot));
+	p->guest_eip = guest_eip;
+	p->host_eip = NULL;
+	p->next = env->shack_hash->head[h];
+	return env->shack_hash->head[h] = p;
+}
 void shack_set_shadow(CPUState *env, target_ulong guest_eip, 
 						unsigned long *host_eip)
 {
+#ifdef HW_OPT_SHACK
+	shack_hash_find(env, guest_eip)->host_eip = host_eip;
+#endif
 }
 
 /*
@@ -45,6 +62,9 @@ void shack_set_shadow(CPUState *env, target_ulong guest_eip,
  */
 void helper_shack_flush(CPUState *env)
 {
+#ifdef HW_OPT_SHACK
+	env->shack_top = env->shack;
+#endif
 }
 
 /*
@@ -53,6 +73,9 @@ void helper_shack_flush(CPUState *env)
  */
 void push_shack(CPUState *env, TCGv_ptr cpu_env, target_ulong next_eip)
 {
+#ifdef HW_OPT_SHACK
+	/* TODO */
+#endif
 }
 
 /*
@@ -61,15 +84,19 @@ void push_shack(CPUState *env, TCGv_ptr cpu_env, target_ulong next_eip)
  */
 void pop_shack(TCGv_ptr cpu_env, TCGv next_eip)
 {
+#ifdef HW_OPT_SHACK
+	/* TODO */
+#endif
 }
 
 
 /*
  * Indirect Branch Target 
  */
+/* in cpu-exec.c to call update_ibtc_entry() */
 __thread int update_ibtc;
 /* Create Indirect Branch Target Cache */
-struct ibtc_table ibtc_cache;
+struct ibtc_table *ibtc_cache;
 
 /*
  * helper_lookup_ibtc()
@@ -78,10 +105,12 @@ struct ibtc_table ibtc_cache;
  */
 void *helper_lookup_ibtc(target_ulong guest_eip)
 {
+#ifdef HW_OPT_IBTC
 	target_ulong hash = guest_eip & IBTC_CACHE_MASK;
-	if (ibtc_cache.htable[hash].guest_eip == guest_eip)
-		return ibtc_cache.htable[hash].tb->tc_ptr;
+	if (ibtc_cache->htable[hash].guest_eip == guest_eip)
+		return ibtc_cache->htable[hash].tb->tc_ptr;
 	update_ibtc = 1;
+#endif
     return optimization_ret_addr;
 }
 
@@ -91,10 +120,12 @@ void *helper_lookup_ibtc(target_ulong guest_eip)
  */
 void update_ibtc_entry(TranslationBlock *tb)
 {
+#ifdef HW_OPT_IBTC
 	update_ibtc = 0;
 	target_ulong hash = tb->pc & IBTC_CACHE_MASK;
-	ibtc_cache.htable[hash].guest_eip = tb->pc;
-	ibtc_cache.htable[hash].tb = tb;
+	ibtc_cache->htable[hash].guest_eip = tb->pc;
+	ibtc_cache->htable[hash].tb = tb;
+#endif
 }
 
 /*
@@ -103,7 +134,10 @@ void update_ibtc_entry(TranslationBlock *tb)
  */
 static inline void ibtc_init(CPUState *env)
 {
+#ifdef HW_OPT_IBTC
 	update_ibtc = 0;
+	ibtc_cache = (struct ibtc_table *) calloc(1, sizeof(struct ibtc_table));
+#endif
 }
 
 /*
