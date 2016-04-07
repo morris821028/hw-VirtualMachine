2eab63f9d40a523c2717a5089ade73a72ee3b141
Shiang-Yun Yang <morris821028@gmail.com>
2016-04-07 10:16:22 +0800

    update my test script

--- qemu-branch-opt/run-benchmark.sh
+++ qemu-branch-opt/run-benchmark.sh
@@ -1,9 +1,11 @@
-time test/indirect-branch/func_ptr.out 10000000
-time build.qemu/i386-linux-user/qemu-i386 test/indirect-branch/func_ptr.out \
-	10000000
-time test/direct-br/basic.out 10000000
-time build.qemu/i386-linux-user/qemu-i386 test/direct-br/basic.out \
-	10000000
-time test/recursion-fib/fib.out 40
-time build.qemu/i386-linux-user/qemu-i386 test/recursion-fib/fib.out \
-	40
+#!/bin/bash
+
+test=("indirect-branch/func_ptr.out 10000000" "direct-br/basic.out 10000000" "recursion-fib/fib.out 40")
+for i in "${test[@]}";
+do
+	echo -e "\033[35m Test ${i} Origin\033[0m";
+	time test/${i}
+	echo -e "\033[35m Test ${i} Qemu\033[0m";
+	time build.qemu/i386-linux-user/qemu-i386 test/${i}
+done
+
